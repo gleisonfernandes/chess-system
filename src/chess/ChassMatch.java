0d6e0eedf1d658278f8527f90d948b66b5ef2982
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-05-07 14:12:32 -0300

    :sparkles: feat: placing pieces on the board

--- src/chess/ChassMatch.java
+++ src/chess/ChassMatch.java
@@ -1,8 +1,9 @@
 package chess;
 
-import java.util.Iterator;
-
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChassMatch {
 	
@@ -10,6 +11,7 @@ public class ChassMatch {
 
 	public ChassMatch() {
 		board = new Board(8,8);
+		initialStup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -21,6 +23,13 @@ public class ChassMatch {
 			
 		}
 		return mat;
+	}
+	
+	private void initialStup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(5, 1));
 	}
 	
 	
